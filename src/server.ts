70073eb2a853b38e10e27a15f7e2e32c9853e258
Zual Rana <freelancerzual@gmail.com>
2023-05-19 14:59:39 +0600

    install cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 const app = express();
 const port = 5000;
+
+app.use(cors());
 
 // database connection
 async function database() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/mongoose-practice");
     console.log(`Database connection successful`);
+    app.listen(port, () => {
+      console.log(`Server running at port ${port}`);
+    });
   } catch (error) {
     console.log(`Database connection failed`, error);
   }
@@ -15,10 +21,6 @@ async function database() {
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Server running at port ${port}`);
 });
 
 database();
