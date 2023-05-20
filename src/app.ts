2e5cfbbcfb3543e23d7c4ed38e8029a96b972ba3
Zual Rana <freelancerzual@gmail.com>
2023-05-20 11:21:38 +0600

    insert data into mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,102 +1,124 @@
-import express, { Application, NextFunction, Request, Response, urlencoded } from "express";
+import express, {
+  Application,
+  NextFunction,
+  Request,
+  Response,
+  urlencoded,
+} from "express";
 import cors from "cors";
 import mongoose, { Schema, model } from "mongoose";
 const app: Application = express();
 // using cors
 app.use(cors());
 // parse data
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
- // inserting a test data into mongodb
- /* 
+  // inserting a test data into mongodb
+  /* 
     step1: Interface
     step2: Schema
     step3: Model
-    step4: Database Query
+    step4: Database Query on model
  */
- 
-    //creating a interface
-    interface IUser {
-      id: string,
-      role: 'student',
-      password: string,
 
+  //creating a interface
+  interface IUser {
+    id: string;
+    role: "student";
+    password: string;
+
+    name: {
+      firstName: string;
+      middleName?: string;
+      lastName: string;
+    };
+    dateOfBirth?: string;
+    gender: "male" | "female" | "other";
+    email: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    presentAddress: string;
+    permanentAddress: string;
+  }
+
+  // creating schema
+  const userSchema = new Schema<IUser>({
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      middleName: {
+        type: String,
+      },
+      lastName: { type: String, required: true },
+    },
+    dateOfBirth: {
+      type: String,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+    },
+
+    email: { type: String, required: true },
+    contactNo: {
+      type: String,
+      required: true,
+    },
+    emergencyContactNo: {
+      type: String,
+      required: true,
+    },
+    presentAddress: {
+      type: String,
+      required: true,
+    },
+    permanentAddress: {
+      type: String,
+      required: true,
+    },
+  });
+
+  // creating model
+  const User = model<IUser>("User", userSchema);
+
+  const createUserToDb = async () => {
+    // creating instance
+    const user = new User({
+      id: "1234",
+      role: "student",
+      password: "1234",
       name: {
-        firstName: string,
-        middleName: string,
-        lastName: string
+        firstName: "zual",
+        lastName: "rana",
       },
-      dateOfBirth?:  string,
-      gender: 'male' | 'female' | 'other',
-        email: string,
-        contactNo: string,
-        emergencyContactNo: string,
-        presentAddress: string,
-        permanentAddress: string,
-    }
-
-    // creating schema
-    const userSchema = new Schema<IUser>({
-      id: {
-        type: String,
-        required:true,
-        unique: true,
-
-      },
-      role: {
-        type: String,
-        required:true
-      },
-      password:{
-        type: String,
-        required:true
-      },
-        name: {
-          firstName: {
-            type: String,
-            required: true
-          },
-          middleName: {
-            type: String,
-
-          },
-          lastName: {type: String, required:true}
-        },
-        dateOfBirth: {
-          type: String
-        },
-        gender: {
-          type : String,
-          required: true
-        },
-
-        email: {type: String, required:true},
-        contactNo: {
-          type: String, 
-          required: true,
-        },
-        emergencyContactNo: {
-          type: String,
-          required: true
-        },
-        presentAddress: {
-          type: String,
-          required: true
-        },
-        permanentAddress: {
-          type: String,
-          required: true
-        }
-    })
-
-    // creating model
-    const User = model<IUser>("User",userSchema)
-
-    User.
-
- 
+      gender: "male",
+      email: "email@.com",
+      contactNo: "017",
+      emergencyContactNo: "016",
+      presentAddress: "jamalpur",
+      permanentAddress: "jamalpur",
+    });
+    await user.save();
+    console.log(user);
+  };
+  createUserToDb();
   // res.send("Hello World!");
   // next();
 });
